bbc9339af8a2b3e60fd5e1bbc54f494dfed1b365
DecmanMedia <diego.martinez.12@sansano.usm.cl>
2018-12-12 19:40:49 -0300

    update non gato

--- src/app/feed/feed.page.ts
+++ src/app/feed/feed.page.ts
@@ -36,8 +36,8 @@ export class FeedPage implements OnInit {private selectedItem: any;
   constructor(private productService: ProductServiceService) {
     for (let i = 1; i < 11; i++) {
       this.items.push({
-        title: 'Gato ' + i,
-        note: 'This is gato #' + i,
+        title: 'item' + i,
+        note: 'This is item #' + i,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
