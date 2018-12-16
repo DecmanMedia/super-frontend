984add32fdc38634b97c28baaadd6b229b3796e1
diego.martinez.12@sansano.usm.cl <diego.martinez.12@sansano.usm.cl>
2018-12-16 20:28:53 -0300

    Search-resulto view workin

--- src/app/search-result/search-result.page.ts
+++ src/app/search-result/search-result.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import {ProductServiceService } from '../product-service.service';
 
 @Component({
@@ -26,11 +26,16 @@ export class SearchResultPage implements OnInit {
 
   private title: String;
   public isSearchbarOpened = false;
+  private searchInput: any="";
   public listProducts: Array<any>;
 
   public items: Array<{ title: string; note: string; icon: string }> = [];
 
-  constructor(private route: ActivatedRoute, private productService: ProductServiceService) {
+  constructor(
+      private route: ActivatedRoute, 
+      private productService: ProductServiceService,
+      private router: Router
+    ) {
     for (let i = 1; i < 11; i++) {
       this.items.push({
         title: 'gato ' + i,
@@ -42,8 +47,15 @@ export class SearchResultPage implements OnInit {
 
   //constructor(private route: ActivatedRoute) { }
 
+  onSearch(){
+    this.router.navigateByUrl(`/search-result/${this.searchInput}`)
+    console.log(this.searchInput);
+  }
+
   ngOnInit(){
     this.title = this.route.snapshot.paramMap.get('name')
+    this.searchInput = this.title
+    console.log(this.title)
     this.productService.getProductsByName(this.title).subscribe(
       data=>{
         console.log(data);
