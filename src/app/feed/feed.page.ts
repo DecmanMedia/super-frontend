6675e7f9aac5418897bfbb427fe7d1b51a44e3de
diego.martinez.12@sansano.usm.cl <diego.martinez.12@sansano.usm.cl>
2018-12-16 20:54:08 -0300

    Add Searchbar to feed view

--- src/app/feed/feed.page.ts
+++ src/app/feed/feed.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavController,NavParams } from '@ionic/angular';
 import { RouterModule, Routes} from '@angular/router'
 import { ProductServiceService } from '../product-service.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-feed',
@@ -26,21 +27,16 @@ export class FeedPage implements OnInit {private selectedItem: any;
   ];
 
   public isSearchbarOpened = false;
-
+  private searchInput: any="";
 
   public listProducts: Array<any>;
 
-
-
   public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor(private productService: ProductServiceService) {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'item' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+  constructor(private productService: ProductServiceService, private router: Router) {
+  }
+  onSearch(){
+    this.router.navigateByUrl(`/search-result/${this.searchInput}`)
+    console.log(this.searchInput);
   }
 
   ngOnInit() {
