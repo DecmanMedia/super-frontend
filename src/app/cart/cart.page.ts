747f03df78e36721bf03c4772cbc48253696b15e
diego.martinez.12@sansano.usm.cl <diego.martinez.12@sansano.usm.cl>
2018-12-17 13:21:46 -0300

    add cart page. Empty. Searchbar functionality.

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductServiceService } from '../product-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.page.scss'],
 })
 export class CartPage implements OnInit {
+  //Search variables
+  public isSearchbarOpened = false;
+  private searchInput: any="";
 
-  constructor() { }
+
+  constructor(
+    private productService: ProductServiceService, 
+    private router: Router
+    ) { }
+
+  onSearch(){
+    this.router.navigateByUrl(`/search-result/${this.searchInput}`)
+    console.log(this.searchInput);
+  }
 
   ngOnInit() {
+    this.searchInput="";
+    this.isSearchbarOpened = false; 
   }
 
 }
